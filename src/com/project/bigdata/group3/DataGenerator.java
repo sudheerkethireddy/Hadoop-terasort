b9bd0cbaec287461735c72dfd453bbe4cf896b84
Kethireddy <skethireddy2@MTVL14025f33e.local>
2016-10-16 22:06:34 -0700

    added util method to reduce I/O thrashing

--- src/com/project/bigdata/group3/DataGenerator.java
+++ src/com/project/bigdata/group3/DataGenerator.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 public class DataGenerator {
 	
+	private final static String filePath = "/Users/skethireddy2/Documents/ITU/Big-Data/Project/InputData/";
+	private final static String fileName = "inputdata.txt";
 	public static void main(String[] args) {
 		try {
-			FileUtil.populateFile();
+			FileUtil.populateFile(filePath,fileName);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/com/project/bigdata/group3/FileUtil.java
+++ src/com/project/bigdata/group3/FileUtil.java
@@ -4,26 +4,31 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileUtil {
 
 	static File file = null;
 	static BufferedWriter bufferWriter = null;
 
-	public static File createFile(String fileName) throws IOException
+	public static File createFile(String filePath,String fileName) throws IOException
 	{
 		if(file==null)
 		{
-			file = new File("/Users/user/Documents/ITU/Big-Data/Project/InputData/"+fileName);
+			Path path = Paths.get(filePath+fileName);
+			Files.createDirectories(path.getParent());
+			file = Files.createFile(path).toFile();
 			return file;
 		}
 		else
 			return file;
 	}
 
-	public static void populateFile() throws IOException
+	public static void populateFile(String filepath, String filename) throws IOException
 	{
-		createFile("inputdata.txt");
+		createFile(filepath,filename);
 		bufferWriter = new BufferedWriter(new FileWriter(file));
 		double bytes = 0.0;
 		double kiloBytes = 0.0;
@@ -35,10 +40,34 @@ public class FileUtil {
 			kiloBytes = bytes/1024;
 			megaBytes = kiloBytes/1024;
 			gigaBytes = megaBytes/1024;	
-			bufferWriter.write(RandomDataGenerator.generateRandomString()+" "+RandomDataGenerator.generateRandomString());
+			if(gigaBytes>=8)
+				System.out.println("gigabyte="+gigaBytes);
+			bufferWriter.write(returnNLinesGeneratedString(200));
 			bufferWriter.newLine();
 		}while(gigaBytes<=10);
 
 
 	}
+	
+	/**
+	 * This util method helps us write n lines at a time to file
+	 * as File I/O is bottlenock for any application
+	 * @param n: the number of lines of string you want to add
+	 * @return string: the string generated
+	 */
+	private static String returnNLinesGeneratedString(int n)
+	{
+		int i = 1;
+		StringBuffer s = new StringBuffer(32);
+		while(i<=n)
+		{
+			s.append(RandomDataGenerator.generateRandomString());
+			s.append(" ");
+			s.append(RandomDataGenerator.generateRandomString());
+			s.append(System.lineSeparator());
+			i++;
+		}
+		return s.toString();
+	}
+	
 }
